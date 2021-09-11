79ccbec247fbcce29594ded3b87b9b81509a9323
shaliniiit <32547466+shaliniiit@users.noreply.github.com>
2021-09-11 16:08:15 +0530

    Added function for Levenshtein Distance to find closest column

--- src/Microsoft.ML.AutoML/Utils/UserInputValidationUtil.cs
+++ src/Microsoft.ML.AutoML/Utils/UserInputValidationUtil.cs
@@ -287,7 +287,7 @@ namespace Microsoft.ML.AutoML
             var closestNamed = string.Empty;
             foreach (var column in trainData.Schema)
             {
-                var editDistance = StringEditDistance.GetLevenshteinDistance(column.Name, columnName);
+                var editDistance = LevenshteinDistance(column.Name, columnName);
                 if (editDistance < minEditDistance)
                 {
                     minEditDistance = editDistance;
@@ -296,6 +296,43 @@ namespace Microsoft.ML.AutoML
             }
 
             return minEditDistance <= maxAllowableEditDistance ? closestNamed : string.Empty;
+        }
+        private int LevenshteinDistance(string source, string target)
+            {
+              if(string.IsNullOrEmpty(source))
+             {
+                if(string.IsNullOrEmpty(target)) 
+                    return 0;
+                return target.Length;
+              }
+              if(string.IsNullOrEmpty(target)) return source.Length;
+
+              if(source.Length > target.Length)
+             {
+                var temp = target;
+                target = source;
+                source = temp;
+              }
+
+              var m = target.Length;
+              var n = source.Length;
+              var distance = new int[2, m + 1];
+
+              for(var j = 1; j <= m; j++) distance[0, j] = j;
+
+              var currentRow = 0;
+              for(var i = 1; i <= n; ++i)
+             {
+                currentRow = i & 1;
+                distance[currentRow, 0] = i;
+                var previousRow = currentRow ^ 1;
+                for(var j = 1; j <= m; j++) 
+                {
+                  var cost = (target[j - 1] == source[i - 1] ? 0 : 1);
+                  distance[currentRow, j] = Math.Min(Math.Min(distance[previousRow, j] + 1,distance[currentRow, j - 1] + 1),  distance[previousRow, j - 1] + cost);
+                }
+              }
+                return distance[currentRow, m];
         }
 
         private static string FindFirstDuplicate(IEnumerable<string> values)
